751387963c499fad7702f7187cfd4b0651cc22be
Huong <whatisor@yahoo.com.vn>
2020-12-04 09:06:07 +0700

    add env port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var temp = 0;
 app.use('/', express.static('.'));
-var server = app.listen(8001, function () {
+var server = app.listen(process.env.PORT || 8001, function () {
   var host = server.address().address;
   var port = server.address().port;
 
